b434d24e5a341fdeee7567a64c81e0465dd86a70
Sami Daik <s@midaik.me>
2024-05-15 13:53:52 +0200

    add quickbooks auth code

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
-const express = require("express");
+
+require('dotenv').config();
+const express = require('express');
+const OAuthClient = require('intuit-oauth');
+const { createClient } = require('@supabase/supabase-js');
+
 const app = express();
 const port = process.env.PORT || 3001;
+
+const oauthClient = new OAuthClient({
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+  environment: process.env.ENVIRONMENT,
+  redirectUri: process.env.REDIRECT_URL
+});
+
+// Initialize Supabase client
+const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_KEY
+);
 
 app.get("/", (req, res) => res.type('html').send(html));
 
@@ -8,6 +26,57 @@ const server = app.listen(port, () => console.log(`Example app listening on port
 
 server.keepAliveTimeout = 120 * 1000;
 server.headersTimeout = 120 * 1000;
+
+app.get('/auth', (req, res) => {
+  const authUri = oauthClient.authorizeUri({
+      scope: [
+          OAuthClient.scopes.Accounting,
+          OAuthClient.scopes.OpenId
+      ],
+      state: 'Init'
+  });
+  res.redirect(authUri);
+});
+
+app.get('/callback', async (req, res) => {
+  const parseRedirect = req.url;
+  try {
+    console.log("parseRedirect");
+    console.log(parseRedirect);
+    oauthClient
+      .createToken(parseRedirect)
+      .then(function (authResponse) {
+        console.log('The Token is  ' + JSON.stringify(authResponse.json));
+        // Store the tokens in the Supabase database
+        const myAsyncFunction = async () => {
+          const { data, error } = await supabase
+            .from('qb_auth')
+            .insert([
+              {
+                access_token: authResponse.getJson().access_token,
+                refresh_token: authResponse.getJson().refresh_token,
+              }
+            ]);
+
+          if (error) {
+            throw error;
+          }
+        };
+
+        myAsyncFunction();
+
+        console.log('Tokens stored successfully:', data);
+        res.send('Authentication successful and tokens stored.');
+      })
+      .catch(function (e) {
+        console.error('The error message is :' + e.originalMessage);
+        console.error(e.intuit_tid);
+  });
+  
+  } catch (err) {
+      console.error(err);
+  }
+});
 
 const html = `
 <!DOCTYPE html>
